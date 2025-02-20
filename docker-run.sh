cc4a8b68648719be89d02011e281034cb8ac38ff
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-20 13:19:58 +0100

    [Fixed] Now topics can be accessed from non-root terminal

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 docker run -it --rm \
   --privileged \
   --net=host \
   -v /dev:/dev/ \
   -v /run/udev/:/run/udev/ \
   --group-add video \
+  --user $(id -u):$(id -g) \
+  -e HOME=/tmp \
   camera_ros \
   ros2 run camera_ros camera_node
-
